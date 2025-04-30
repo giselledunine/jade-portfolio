8f5c17a9e5827a9abb3c3c76e25e98877cf17208
giselledunine <sophia.hmamouche@outlook.com>
2025-04-30 12:22:19 +0200

    add portal animation and presentation of projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
-import { useEffect, useRef, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
-import { ScrollControls, Scroll } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import {
+    ScrollControls,
+    Scroll,
+    MeshPortalMaterial,
+    Text,
+    CameraControls,
+    Preload,
+    useScroll,
+} from "@react-three/drei";
 // import { Slider } from "./components/ui/slider";
 // import { useProgress } from "@react-three/drei";
 import gsap from "gsap";
+import * as THREE from "three";
 
 function App() {
     const [value, setValue] = useState(0);
     const [duration, setDuration] = useState(0);
+    const [active, setActive] = useState(0);
     const videoRef = useRef<HTMLVideoElement>(null);
-    const timeline = gsap.timeline();
+    const loadingDivRef = useRef<HTMLDivElement>(null);
+    const nameRef = useRef<HTMLElement>(null);
+    const regular = import("@/fonts/MinionPro-Bold.otf");
     // const { progress } = useProgress();
 
     const handleLoadedMetadata = () => {
@@ -33,12 +45,30 @@ function App() {
     // };
 
     useEffect(() => {
+        if (value == 100) {
+            gsap.to(loadingDivRef.current, {
+                transform: "translateY(-100vh)",
+                duration: 1,
+                ease: "power1.inOut",
+            });
+            // gsap.to(nameRef.current, {
+            //     transform: "translate(-35vw, -40vh)",
+            //     scale: 0.3,
+            //     duration: 2,
+            //     ease: "power1.out",
+            //     delay: 2,
+            // });
+        }
+    }, [value]);
+
+    useEffect(() => {
+        const timeline = gsap.timeline();
         if (videoRef.current) {
             timeline.to(
                 videoRef.current,
                 {
                     currentTime: (23 * duration) / 100,
-                    duration: 0.5,
+                    duration: 0.3,
                     ease: "sine.inOut",
                     onUpdate: () => {
                         if (videoRef.current) {
@@ -57,7 +87,7 @@ function App() {
                 videoRef.current,
                 {
                     currentTime: (68 * duration) / 100,
-                    duration: 1.5,
+                    duration: 0.9,
                     ease: "sine.inOut",
                     onUpdate: () => {
                         if (videoRef.current) {
@@ -70,7 +100,7 @@ function App() {
                         }
                     },
                 },
-                0.7
+                0.4
             );
             timeline.to(
                 videoRef.current,
@@ -89,7 +119,7 @@ function App() {
                         }
                     },
                 },
-                2.4
+                1.4
             );
             timeline.to(
                 videoRef.current,
@@ -108,18 +138,32 @@ function App() {
                         }
                     },
                 },
-                3.1
+                1.9
             );
         }
     }, [duration]);
 
     return (
         <div className="w-[100vw] h-[100vh] touch-pan-y">
-            <Canvas>
-                {/* <color attach="background" args={["000000"]} /> */}
+            <Canvas camera={{ fov: 75, position: [0, 0, 5] }}>
+                {/* <CameraControls enabled={cameraMouv}></CameraControls> */}
+                <color attach="background" args={["#F2FFF4"]} />
                 <ScrollControls damping={0.3} pages={5} enabled={true}>
-                    <Scroll html style={{ width: "100%" }}>
-                        <div className="flex flex-col justify-center items-center w-[100vw] h-[100vh] bg-black">
+                    <Scroll>
+                        <ProjectPortal
+                            active={active}
+                            setActive={setActive}></ProjectPortal>
+                    </Scroll>
+                    <Scroll
+                        html
+                        style={{
+                            width: "100%",
+                            scrollbarWidth: "none",
+                            msOverflowStyle: "none",
+                        }}>
+                        <div
+                            ref={loadingDivRef}
+                            className="absolute flex flex-col justify-center items-center w-[100vw] h-[100vh] bg-black">
                             <video
                                 ref={videoRef}
                                 src={`${window.location.origin}/jadeStarAnimation.mp4`}
@@ -136,21 +180,118 @@ function App() {
                                 min={0}
                                 step={0.01}
                                 onValueChange={handleSlideChange}></Slider> */}
-                            <h1 className="text-white text-4xl font-display italic">
+                            <h1 className="text-white text-3xl font-display italic">
                                 {value}%
                             </h1>
                         </div>
-                        <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-white">
-                            <h1 className="text-black">Kaidi</h1>
+                        <div className="flex justify-center items-center w-[100vw] h-[100vh]">
+                            {/* <h1
+                                ref={nameRef}
+                                className="text-black text-9xl font-display">
+                                Kaïdi Jade
+                            </h1> */}
                         </div>
                         <div className="flex justify-center items-center w-[100vw] h-[100vh] bg-black">
                             <h1 className="text-white">Portfolio</h1>
                         </div>
                     </Scroll>
                 </ScrollControls>
+                <CameraMouv active={active}></CameraMouv>
+                <Preload all></Preload>
             </Canvas>
         </div>
     );
 }
+
+function CameraMouv({ active }: { active: number }) {
+    const { camera, scene } = useThree();
+    useEffect(() => {
+        const object = scene.getObjectByName("01");
+        console.log("active", active);
+        if (active === 1 && object) {
+            gsap.to(camera.position, {
+                x: 0,
+                y: 0,
+                z: 2,
+                duration: 0.8,
+                ease: "power3.inOut",
+                onUpdate: () => {
+                    camera.lookAt(0, 0, 0);
+                },
+            });
+        }
+    });
+    return <></>;
+}
+
+const ProjectPortal = ({
+    active,
+    setActive,
+}: {
+    active: number;
+    setActive: Dispatch<SetStateAction<number>>;
+}) => {
+    const scroll = useScroll();
+    console.log("scroll", scroll);
+    const portalMaterialRef = useRef<typeof MeshPortalMaterial>(null);
+    const handleActive = (id: number) => {
+        setActive(id);
+    };
+
+    useEffect(() => {
+        if (active === 1) {
+            gsap.to(portalMaterialRef.current, {
+                blend: 1,
+                duration: 0.3,
+                delay: 0.1,
+                ease: "power1.inOut",
+            });
+        }
+    }, [active]);
+
+    return (
+        <group>
+            <Text
+                font="/MinionPro-Bold.otf"
+                fontSize={0.4}
+                color={"#F0F0F0"}
+                position={[-0.2, 1.7, 0.01]}
+                material-toneMapped={false}
+                anchorY="top"
+                anchorX="right">
+                La Liste Rouge
+            </Text>
+            <Text
+                font="/MinionPro-Bold.otf"
+                fontSize={0.2}
+                color={"#F0F0F0"}
+                lineHeight={0.8}
+                position={[-2.25, 1.2, 0.01]}
+                material-toneMapped={false}
+                anchorY="top"
+                anchorX="right">
+                2023
+            </Text>
+            <mesh
+                name={"01"}
+                position={[0, 0, 0]}
+                onDoubleClick={() => handleActive(1)}>
+                <planeGeometry args={[6, 4]} />
+                <MeshPortalMaterial
+                    ref={portalMaterialRef}
+                    side={2}
+                    resolution={512}
+                    blur={0}>
+                    <color attach="background" args={["#000000"]} />
+
+                    <mesh position={[0, 0, 0]}>
+                        <planeGeometry args={[1, 1, 1]} />
+                        <meshBasicMaterial color={"#A4564C"} />
+                    </mesh>
+                </MeshPortalMaterial>
+            </mesh>
+        </group>
+    );
+};
 
 export default App;
