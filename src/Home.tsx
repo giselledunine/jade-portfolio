3e15557e6152f1e93a4ec0666c9f0bc6e8ad450a
giselledunine <sophia.hmamouche@outlook.com>
2025-05-07 22:46:41 +0200

    fix build errors

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,4 @@
-import { Text, Scroll } from "@react-three/drei";
+import { Scroll } from "@react-three/drei";
 import gsap from "gsap";
 import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import * as THREE from "three";
@@ -153,61 +153,5 @@ export default function Home() {
                     transparent={true}></meshBasicMaterial>
             </mesh>
         </>
-
-        // <group>
-        //     <Text
-        //         font="/Helvetica.ttf"
-        //         color={"#000"}
-        //         scale={0.4}
-        //         anchorX={"left"}
-        //         position={[-4, 2.5, 0]}>
-        //         Kaïdi Jade
-        //     </Text>
-        //     <Text
-        //         font="/Helvetica.ttf"
-        //         color={"#000"}
-        //         scale={0.2}
-        //         anchorX={"right"}
-        //         position={[4, 2.5, 0]}>
-        //         Directrice Artistique
-        //     </Text>
-        //     <Text
-        //         font="/Helvetica.ttf"
-        //         color={"#000"}
-        //         scale={0.2}
-        //         anchorX={"right"}
-        //         position={[4, 2.2, 0]}>
-        //         Designer Graphique
-        //     </Text>
-        //     <Text
-        //         font="/Helvetica.ttf"
-        //         color={"#000"}
-        //         scale={0.2}
-        //         anchorX={"right"}
-        //         position={[4, 1.9, 0]}>
-        //         Photographe
-        //     </Text>
-        //     <Text
-        //         font="/Helvetica.ttf"
-        //         color={"#000"}
-        //         scale={0.2}
-        //         anchorX={"right"}
-        //         position={[4, 1.6, 0]}>
-        //         UX/UI
-        //     </Text>
-        //     <Text
-        //         font="/MinionPro-Medium.otf"
-        //         color={"#000"}
-        //         scale={0.2}
-        //         anchorX={"left"}
-        //         position={[-4, 0.5, 0]}>
-        //         Diplômée d'un master en direction artistique et design graphique
-        //         au Campus de la fonderie de l'image en 2023, je suis une
-        //         directrice artistique et graphiste avec 2 ans d'expérience en
-        //         alternance en tant que UX/UI junior au sein d'une agence de
-        //         digitale. J’interviens sur des projets variés allant de la
-        //         réalisation d'un logo à celui d'un site web.
-        //     </Text>
-        // </group>
     );
 }
